9261c22da6e548007179f24c07c84e5afd9a6037
pankaj-deepnapsoftech <itsybizz@gmail.com>
2025-12-16 11:30:17 +0530

    Some change

--- src/middleware/Authorization.js
+++ src/middleware/Authorization.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 // --------------------------- local imports ----------------------------
 import { AsyncHandler } from "../utils/asyncHandler.js";
-import { BadRequestError } from "../utils/errorHandler.js";
+import { BadRequestError, NotFoundError } from "../utils/errorHandler.js";
 import { FindUserById } from "../services/users.service.js";
 import { config } from "../config.js";
 
@@ -17,6 +17,11 @@ export const Authorization = AsyncHandler(async (req,res,next) => {
     const payload = jwt.verify(token,config.JWT_SECRET);
 
     const user = await FindUserById(payload.id);
+
+    if(user.terminate){
+        throw new NotFoundError("User Terminated by Admin Please Contact to organization","Authorization() method error")
+    }
+
     req.currentUser = user;
     next()
 })
