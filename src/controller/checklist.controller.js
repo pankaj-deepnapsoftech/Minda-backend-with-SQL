d1233d19f1f4c2853744ce523ed83dbeac873f4f
nitinthakurdev <radheradhe9870023@gmail.com>
2026-01-14 11:48:36 +0530

    new chnages by nitin dev

--- src/controller/checklist.controller.js
+++ src/controller/checklist.controller.js
@@ -18,12 +18,12 @@ const __dirname = path.dirname(__filename);
 export const CreateChecklistData = AsyncHandler(async (req, res) => {
         const data = req.body;
         const file = req.file;
-    
         const file_path = file ? `${config.NODE_ENV !== "development" ? config.SERVER_URL : config.LOCAL_SERVER_URL}/files/${file.filename}` : null;
         const parsTime = JSON.parse(data.time);
+    
 
     
-        const result = await createChecklistService(file_path ? { ...data, file_path, total_checks: JSON.parse(data.time) } : { ...data, total_checks: parsTime?.length || 0 });
+    const result = await createChecklistService(file_path ? { ...data, file_path, total_checks: parsTime?.length || 0 } : { ...data, total_checks: parsTime?.length || 0 });
         if(data?.time){
             const mapData = parsTime.map((timeItem)=>({check_time:timeItem,item_id:result._id}));
             await createChecklistItemTimeService(mapData);
